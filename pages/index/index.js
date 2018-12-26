f86936082349708c5942d986a5e4141da24ab21f
fanghonghui <369545745@qq.com>
2018-12-26 15:02:27 +0800

    dd

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,13 @@
 //index.js
+var API = require('../../lib/api.js');
 const app = getApp()
 
 Page({
   data: {
   },
   onLoad: function () {
-
+      API.post('query',(res)=>{
+        console.log('请求成功',res)
+      })
   }
 })
